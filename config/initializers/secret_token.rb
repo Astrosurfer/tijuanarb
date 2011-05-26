93f9f6b6e08ca27f4dffd5905cda005cc8c59b15
Fernando Castellanos <octavo_sentido@yahoo.com>
2011-05-26 09:02:04 -0700

    Primer commit, apenas se creo la estructura de aplicación con refinerycms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tijuanarb::Application.config.secret_token = 'c5797f6b5d947cfaf9124cdab9bf8a55e8c5385616e4eb092462200773988285aed050879c2f7d83efbbfcef7deafdc227c8984d24434d4c4e944a375e6645b4'
